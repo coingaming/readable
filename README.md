dfc260d0554db80353727b85717af263bc9d6491
Ilja Tkachuk <ilja@heathmont.net>
2019-07-10 16:00:43 +0300

    dev tools

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
 # Readable
 
-**TODO: Add description**
+<img src="priv/img/logo.png" width="300"/>
 
 ## Installation
 
-If [available in Hex](https://hex.pm/docs/publish), the package can be installed
-by adding `readable` to your list of dependencies in `mix.exs`:
+If the package can be installed by adding `readable` to your list of dependencies in `mix.exs`:
 
 ```elixir
 def deps do
@@ -14,8 +13,3 @@ def deps do
   ]
 end
 ```
-
-Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
-and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
-be found at [https://hexdocs.pm/readable](https://hexdocs.pm/readable).
-
--- mix.exs
+++ mix.exs
@@ -4,11 +4,47 @@ defmodule Readable.MixProject do
   def project do
     [
       app: :readable,
-      version: "0.1.0",
+      version: "VERSION" |> File.read!() |> String.trim(),
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       consolidate_protocols: Mix.env() != :test,
-      deps: deps()
+      deps: deps(),
+      aliases: aliases(),
+      # excoveralls
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.travis": :test,
+        "coveralls.circle": :test,
+        "coveralls.semaphore": :test,
+        "coveralls.post": :test,
+        "coveralls.detail": :test,
+        "coveralls.html": :test
+      ],
+      # dialyxir
+      dialyzer: [
+        ignore_warnings: ".dialyzer_ignore",
+        plt_add_apps: [
+          :mix,
+          :ex_unit
+        ]
+      ],
+      # ex_doc
+      name: "Readable",
+      source_url: "https://github.com/coingaming/readable",
+      homepage_url: "https://github.com/coingaming/readable",
+      docs: [main: "readme", extras: ["README.md"]],
+      # hex.pm stuff
+      description: "read type class families",
+      package: [
+        licenses: ["Apache 2.0"],
+        files: ["lib", "priv", "mix.exs", "README*", "VERSION*"],
+        maintainers: ["ILJA TKACHUK"],
+        links: %{
+          "GitHub" => "https://github.com/coingaming/readable",
+          "Author's home page" => "http://itkach.uk"
+        }
+      ]
     ]
   end
 
@@ -22,9 +58,19 @@ defmodule Readable.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:typable, "~> 0.3"}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:typable, "~> 0.3"},
+      # development tools
+      {:excoveralls, "~> 0.11", runtime: false, only: [:dev, :test]},
+      {:dialyxir, "~> 0.5", runtime: false, only: [:dev, :test]},
+      {:ex_doc, "~> 0.19", runtime: false, only: [:dev, :test]},
+      {:credo, "~> 0.9", runtime: false, only: [:dev, :test]},
+      {:boilex, "~> 0.2", runtime: false, only: [:dev, :test]}
+    ]
+  end
+
+  defp aliases do
+    [
+      docs: ["docs", "cmd mkdir -p doc/priv/img/", "cmd cp -R priv/img/ doc/priv/img/", "docs"]
     ]
   end
 end
